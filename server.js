df037208f47b71ebaf2e10e847c8f00a85ca3fd8
oleksandr-diachenko <mail.positiv@gmail.com>
2019-12-06 16:35:29 +0200

    настройки для запуска на хероку попытка номер 10

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 app.use(express.static(__dirname + '/price-checker-client'));
 app.get('/*', function(req,res){
-res.sendFile(path.join('dist/price-checker-client/', 'index.html'))
+res.sendFile(path.join(__dirname + '/dist/price-checker-client/', 'index.html'))
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
