129f2244fa2f861c74f520e4d6fc3a78333894a4
oleksandr-diachenko <mail.positiv@gmail.com>
2019-12-06 13:50:23 +0200

    настройки для запуска на хероку попытка номер 8

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/dist'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/price-checker-client/index.html'));
 });
 
 // default Heroku PORT
