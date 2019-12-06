f37f2f8397c71e7fe113038f3d4b4ea78ac09a55
oleksandr-diachenko <mail.positiv@gmail.com>
2019-12-06 13:03:42 +0200

    настройки для запуска на хероку попытка номер 5

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/magazine-price-checker'));
+app.use(express.static(__dirname + '/dist/price-checker-client'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/magazine-price-checker/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/price-checker-client/index.html'));
 });
 
 // default Heroku PORT
