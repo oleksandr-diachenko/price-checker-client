2d7c0642390eaf798e21f7ab3dc330e91e3d9e5a
oleksandr-diachenko <mail.positiv@gmail.com>
2019-12-09 13:59:36 +0200

    настройки для запуска на хероку попытка номер

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+var express  = require('express');
+var app      = express();
+var httpProxy = require('http-proxy');
+var apiProxy = httpProxy.createProxyServer();
+var backend = 'https://price-checker-web.herokuapp.com/',
+    frontend = 'https://magazine-price-checker.herokuapp.com/';
 
-app.get('/api/*', function(req,res) {
-  //modify the url in any way you want
-  var newurl = 'https://price-checker-web.herokuapp.com/';
-  request(newurl).pipe(res);
+app.all("/api/*", function(req, res) {
+  apiProxy.web(req, res, {target: backend});
 });
 
+app.all("/*", function(req, res) {
+    apiProxy.web(req, res, {target: frontend});
+});
 
-//// Serve static files....
-//app.use(express.static(__dirname + '/dist/magazine-price-checker'));
-//
-//// Send all requests to index.html
-//app.get('/*', function(req, res) {
-//  res.sendFile(path.join(__dirname + '/dist/magazine-price-checker/index.html'));
-//});
-
-// default Heroku PORT
-app.listen(process.env.PORT || 3000);
+var server = require('http').createServer(app);
+server.on('upgrade', function (req, socket, head) {
+  apiProxy.ws(req, socket, head, {target: frontend});
+});
+server.listen(3000);
