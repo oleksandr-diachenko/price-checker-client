003d29a1e88aa25b20ea10a17ddcef6692455883
oleksandr-diachenko <mail.positiv@gmail.com>
2019-12-09 13:51:40 +0200

    настройки для запуска на хероку попытка номер

--- server.js
+++ server.js
@@ -2,13 +2,20 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files....
-app.use(express.static(__dirname + '/dist/magazine-price-checker'));
-
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/magazine-price-checker/index.html'));
+app.get('/api/*', function(req,res) {
+  //modify the url in any way you want
+  var newurl = 'https://price-checker-web.herokuapp.com/';
+  request(newurl).pipe(res);
 });
+
+
+//// Serve static files....
+//app.use(express.static(__dirname + '/dist/magazine-price-checker'));
+//
+//// Send all requests to index.html
+//app.get('/*', function(req, res) {
+//  res.sendFile(path.join(__dirname + '/dist/magazine-price-checker/index.html'));
+//});
 
 // default Heroku PORT
 app.listen(process.env.PORT || 3000);
