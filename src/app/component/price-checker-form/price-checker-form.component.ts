9169e821ab2fdb21dfdc2537fb0d42e7f8f900fd
alexander-diachenko <mail.positiv@gmail.com>
2019-12-10 23:33:42 +0200

    заменил героя на форму ввода и добавил загрузку

--- src/app/component/price-checker-form/price-checker-form.component.ts
+++ src/app/component/price-checker-form/price-checker-form.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpEventType } from '@angular/common/http';
 import { saveAs } from 'file-saver';
 import { PriceService } from 'app/service/price-service/price.service';
-import { Hero }    from 'app/model/hero/hero';
+import { InputForm }    from 'app/model/input-form/input-form';
 
 @Component({
     selector: 'app-price-checker-form',
@@ -11,46 +11,49 @@ import { Hero }    from 'app/model/hero/hero';
 })
 export class PriceCheckerFormComponent implements OnInit {
 
-    powers = ['Really Smart', 'Super Flexible',
-              'Super Hot', 'Weather Changer'];
+    inputForm: InputForm = new InputForm(null, 1, 1);
 
-    model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+    submitted: boolean = false;
 
-    submitted = false;
+    filePicked: boolean = false;
 
-    onSubmit() { this.submitted = true; }
+    constructor(private priceService: PriceService) { }
 
-    newHero() {
-      this.model = new Hero(42, '', '');
+    onSubmit() {
+        this.submitted = true;
     }
 
-   //fileBefore: File;
-   //fileAfter: File;
-   //isDownloadDisable: boolean = true;
+    ngOnInit() {
+    }
 
-   //constructor(private http: HttpClient, private priceService: PriceService) { }
+    fileProgress(fileInput: any) {
+        this.inputForm.file = <File>fileInput.target.files[0];
+        if(this.inputForm.file != null) {
+            this.filePicked = true;
+        } else {
+            this.filePicked = false;
+        }
+    }
 
-   ngOnInit() {
+    checkPrice() {
+        const formData = new FormData();
+        console.log(this.inputForm.file)
+        formData.append('file', this.inputForm.file);
+        this.priceService.getPriceTable(formData).subscribe((data) => {
+            var b: any = new Blob([data], { type: 'application/binary' });
+            console.log(b)
+            b.lastModifiedDate = new Date();
+            b.name = this.getCurrentFileName(this.inputForm.file.name);
+            this.inputForm.file = <File>b;
+            console.log(this.inputForm.file)
+        });
+    }
+
+    getCurrentFileName(fileName: string) {
+        return new Date().valueOf() + '_' + this.inputForm.file.name;
+    }
+
+   download() {
+       saveAs(this.inputForm.file, this.getCurrentFileName(this.inputForm.file.name))
    }
-
-   //fileProgress(fileInput: any) {
-   //    this.fileBefore = <File>fileInput.target.files[0];
-   //}
-
-   //checkPrice() {
-   //    const formData = new FormData();
-   //    formData.append('file', this.fileBefore);
-   //    this.isDownloadDisable = true;
-   //    this.priceService.getPriceTable(formData).subscribe((data) => {
-   //        var b: any = new Blob([data], { type: 'application/binary' });
-   //        b.lastModifiedDate = new Date();
-   //        b.name = this.fileBefore.name;
-   //        this.fileAfter = <File>b;
-   //        this.isDownloadDisable = false;
-   //    });
-   //}
-
-   //download() {
-   //    saveAs(this.fileAfter, new Date().valueOf() + '_' + this.fileBefore.name)
-   //}
 }
