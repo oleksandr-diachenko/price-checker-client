6224025fc9439ca1b4a59d0e819ce7044f3bc2cb
oleksandr-diachenko <mail.positiv@gmail.com>
2019-12-11 16:00:25 +0200

    убрал лишний модуль

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PriceTableComponent } from './component/price-table/price-table.component';
 
-
-const routes: Routes = [{path:'price-table', component: PriceTableComponent}];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { PriceTableComponent } from './component/price-table/price-table.component';
 import { PriceCheckerFormComponent } from './component/price-checker-form/price-checker-form.component';
+import { PriceService } from './service/price-service/price.service';
 import { FormsModule }   from '@angular/forms';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { LoaderComponent } from './component/loader/loader.component'
@@ -16,7 +16,6 @@ import { FooterComponent } from './component/footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
-    PriceTableComponent,
     PriceCheckerFormComponent,
     LoaderComponent,
     HeaderComponent,
@@ -30,7 +29,8 @@ import { FooterComponent } from './component/footer/footer.component';
     MatProgressSpinnerModule
   ],
   providers: [
-    LoaderService
+    LoaderService,
+    PriceService
   ],
   bootstrap: [AppComponent]
 })
