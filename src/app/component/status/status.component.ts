5f0145cdf6f59d6c12c2528951d004b9284eac0e
oleksandr-diachenko <mail.positiv@gmail.com>
2020-01-06 13:24:44 +0200

    временно прописал конекшн к хероку в коде

--- src/app/component/status/status.component.ts
+++ src/app/component/status/status.component.ts
@@ -13,7 +13,7 @@ import { PriceService } from 'app/service/price-service/price.service';
 })
 export class StatusComponent implements OnInit{
 
-  private serverUrl = 'http://localhost:8080/socket'
+  private serverUrl = 'https://price-checker-web.herokuapp.com/socket'
   private title = 'WebSockets chat';
   private stompClient;
 
