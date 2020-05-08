ad767ccd9fdc11268fe38225d51cedeee52a266a
Oleksandr Diachenko <mail.positiv@gmail.com>
2020-05-08 13:53:04 +0300

    [Resolve #45] Change endpoint from file to files (#46)

--- src/app/service/price-service/price.service.ts
+++ src/app/service/price-service/price.service.ts
@@ -15,7 +15,7 @@ export class PriceService {
     }
 
     public getFileById(id: number) {
-        return this.httpClient.get('/api/pricecheck/file/' + id, {responseType: 'arraybuffer'});
+        return this.httpClient.get('/api/pricecheck/files/' + id, {responseType: 'arraybuffer'});
     }
 
     public getFileStatuses() {
