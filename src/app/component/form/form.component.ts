173557910c616aeb4ab858a578b11f9b4560adc2
oleksandr-diachenko <mail.positiv@gmail.com>
2020-06-03 19:36:35 +0300

    [Resolve #51] Add users and auth

--- src/app/component/form/form.component.ts
+++ src/app/component/form/form.component.ts
@@ -1,49 +1,76 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpErrorResponse} from '@angular/common/http';
 import {PriceService} from 'app/service/price.service';
-import {Form} from 'app/model/form';
-import {LoaderService} from 'app/service/loader.service';
 import {SnackBarService} from '../../service/snack-bar.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AlertService} from '../../service/alert.service';
 
 @Component({
     selector: 'app-form',
     templateUrl: './form.component.html',
     styleUrls: ['./form.component.scss']
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
 
-    inputForm = new Form(null, 1, 1);
+    priceCheckForm: FormGroup;
+    loading = false;
+    submitted = false;
 
-    constructor(private priceService: PriceService, private loaderService: LoaderService, private snackBar: SnackBarService) {
-    }
+    private priceService: PriceService;
+    private formBuilder: FormBuilder;
+    private snackBar: SnackBarService;
+    private alertService: AlertService;
 
-    public fileProgress(fileInput: any): void {
-        this.inputForm.file = fileInput.target.files[0] as File;
-        this.changeFileInputLabel();
-    }
-
-    public checkPrice(): void {
-        this.loaderService.show();
-        const formData = new FormData();
-        formData.append('file', this.inputForm.file);
-        this.priceService.processFile(formData, this.inputForm.urlColumn, this.inputForm.insertColumn)
-            .subscribe(() => this.handleResponse(),
-                error => this.handleError(error));
-    }
-
-    private changeFileInputLabel(): void {
-        document.getElementById('fileInputLabel').innerHTML = this.inputForm.file
-            ? this.inputForm.file.name
-            : 'Choose file *';
+    constructor(priceService: PriceService, formBuilder: FormBuilder, snackBar: SnackBarService, alertService: AlertService) {
+        this.alertService = alertService;
+        this.snackBar = snackBar;
+        this.formBuilder = formBuilder;
+        this.priceService = priceService;
     }
 
     private handleResponse(): void {
-        this.loaderService.hide();
+        this.loading = false;
         this.snackBar.openGreenSnackBar('Successfully added to queue!');
     }
 
     private handleError(error: HttpErrorResponse): void {
-        this.loaderService.hide();
+        this.loading = false;
         this.snackBar.openRedSnackBar(error.message);
+    }
+
+    ngOnInit(): void {
+        this.priceCheckForm = this.formBuilder.group({
+            file: ['', Validators.required],
+            fileSource: ['', [Validators.required]],
+            urlColumn: ['', [Validators.required, Validators.min(1)]],
+            insertColumn: ['', [Validators.required, Validators.min(1)]]
+        });
+    }
+
+    onSubmit() {
+        this.submitted = true;
+        this.alertService.clear();
+        if (this.priceCheckForm.invalid) {
+            return;
+        }
+        this.loading = true;
+        const formData = new FormData();
+        formData.append('file', this.priceCheckForm.get('fileSource').value);
+        this.priceService.processFile(formData, this.f.urlColumn.value, this.f.insertColumn.value)
+            .subscribe(() => this.handleResponse(),
+                error => this.handleError(error));
+    }
+
+    get f() {
+        return this.priceCheckForm.controls;
+    }
+
+    onFileChange(event) {
+        if (event.target.files.length > 0) {
+            const file = event.target.files[0];
+            this.priceCheckForm.patchValue({
+                fileSource: file
+            });
+        }
     }
 }
